fce39d809c6502076bec707920bc3b1501ba7576
Baorun Mu <bmu@nvidia.com>
2026-01-28 19:12:22 -0500

    distinguish encoder and lm for graph capture range.

--- vllm/model_executor/models/qwen3_vl.py
+++ vllm/model_executor/models/qwen3_vl.py
@@ -366,7 +366,7 @@ class Qwen3_VisionTransformer(nn.Module):
 
         from vllm.compilation.backends import set_model_tag
 
-        with set_model_tag("Qwen3_VisionPatchEmbed"):
+        with set_model_tag("Qwen3_VisionPatchEmbed", is_encoder=True):
             self.patch_embed = Qwen3_VisionPatchEmbed(
                 patch_size=self.patch_size,
                 temporal_patch_size=self.temporal_patch_size,
@@ -385,7 +385,7 @@ class Qwen3_VisionTransformer(nn.Module):
             rope_parameters={"partial_rotary_factor": 0.5},
         )
 
-        with set_model_tag("Qwen3_VisionPatchMerger"):
+        with set_model_tag("Qwen3_VisionPatchMerger", is_encoder=True):
             self.merger = Qwen3_VisionPatchMerger(
                 d_model=vision_config.out_hidden_size,
                 context_dim=self.hidden_size,
@@ -396,7 +396,7 @@ class Qwen3_VisionTransformer(nn.Module):
                 prefix=f"{prefix}.merger",
             )
 
-        with set_model_tag("Qwen3_VisionPatchMerger_postshuffle_norm"):
+        with set_model_tag("Qwen3_VisionPatchMerger_postshuffle_norm", is_encoder=True):
             self.deepstack_merger_list = nn.ModuleList(
                 [
                     Qwen3_VisionPatchMerger(
@@ -431,7 +431,7 @@ class Qwen3_VisionTransformer(nn.Module):
             raise RuntimeError(
                 f"Qwen3-VL does not support {self.attn_backend} backend now."
             )
-        with set_model_tag("Qwen3_VisionBlock"):
+        with set_model_tag("Qwen3_VisionBlock", is_encoder=True):
             self.blocks = nn.ModuleList(
                 [
                     Qwen3_VisionBlock(
